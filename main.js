c9be92da415c877554477fe317b61f3609f34d54
Jain <39899282+jain33@users.noreply.github.com>
2019-01-16 11:47:13 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/health', (req, res) => res.send('i am aliving'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
